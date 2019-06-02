5acd775f9638b4970fa8eee1e470e38cb29273eb
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-06-02 20:37:36 +0900

    Enable JSON encoding/decoding (lein)

--- minimal-api-lein/project.clj
+++ minimal-api-lein/project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[integrant "0.7.0"]
+  :dependencies [[camel-snake-kebab "0.4.0"]
+                 [integrant "0.7.0"]
                  [org.clojure/clojure "1.10.0"]
                  [ring/ring-core "1.7.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-json "0.4.0"]]
   :main ^:skip-aot minimal-api-lein.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- minimal-api-lein/src/minimal_api_lein/core.clj
+++ minimal-api-lein/src/minimal_api_lein/core.clj
@@ -1,16 +1,38 @@
 (ns minimal-api-lein.core
   (:gen-class)
-  (:require [integrant.core :as ig]
+  (:require [camel-snake-kebab.core :refer [->kebab-case ->snake_case]]
+            [camel-snake-kebab.extras :refer [transform-keys]]
+            [integrant.core :as ig]
             [ring.adapter.jetty :as jetty]
+            [ring.middleware.json :refer [wrap-json-params wrap-json-response]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.params :refer [wrap-params]]
             [ring.util.response :as response]))
 
 ;;; handlers
 
 (defn hello-world [request]
-  (response/response "Hello, World!"))
+  ;; for debug
+  (clojure.pprint/pprint (:params request))
+  (response/response {:message "Hello, World!"
+                      :params (:params request)}))
+
+;;; middleware
+
+(defn wrap-kebab-case-keys [handler]
+  (fn [request]
+    (let [response (-> request
+                       (update :params (partial transform-keys #(->kebab-case % :separator \_)))
+                       handler)]
+      (transform-keys #(->snake_case % :separator \-) response))))
 
 (defmethod ig/init-key ::app [_ _]
-  hello-world)
+  (-> hello-world
+      wrap-kebab-case-keys
+      wrap-keyword-params
+      wrap-json-params
+      wrap-json-response
+      wrap-params))
 
 ;;; API server
 
