9b7c182004c0bec44492468ae8963aef8a99807f
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-06-02 22:22:14 +0900

    Enable routing & implement handlers (lein)

--- minimal-api-lein/project.clj
+++ minimal-api-lein/project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[camel-snake-kebab "0.4.0"]
+  :dependencies [[bidi "2.1.6"]
+                 [camel-snake-kebab "0.4.0"]
                  [integrant "0.7.0"]
+                 [metosin/ring-http-response "0.9.1"]
                  [org.clojure/clojure "1.10.0"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
