7bc9a4a31acbaf00c7f76d2436a8e891a961b2cd
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-06-02 17:10:22 +0900

    Minimal API implementation (lein)

--- minimal-api-lein/project.clj
+++ minimal-api-lein/project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[integrant "0.7.0"]
+                 [org.clojure/clojure "1.10.0"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]]
   :main ^:skip-aot minimal-api-lein.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- minimal-api-lein/src/minimal_api_lein/core.clj
+++ minimal-api-lein/src/minimal_api_lein/core.clj
@@ -1,7 +1,34 @@
 (ns minimal-api-lein.core
-  (:gen-class))
+  (:gen-class)
+  (:require [integrant.core :as ig]
+            [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;;; handlers
+
+(defn hello-world [request]
+  (response/response "Hello, World!"))
+
+(defmethod ig/init-key ::app [_ _]
+  hello-world)
+
+;;; API server
+
+(defmethod ig/init-key ::server [_ {:keys [app options]}]
+  (jetty/run-jetty app options))
+
+(defmethod ig/halt-key! ::server [_ server]
+  (.stop server))
+
+;;; system configuration
+
+(def config
+  {::app {}
+   ::server {:app (ig/ref ::app)
+             :options {:port 3000
+                       :join? false}}})
+
+;;; main entry point
+
+(defn -main [& args]
+  (ig/init config))
