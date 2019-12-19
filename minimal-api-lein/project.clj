b6687ee46f88751a2e61e8c90d69c803e7fcf636
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-12-19 12:21:23 +0900

    Update dependencies

--- minimal-api-lein/project.clj
+++ minimal-api-lein/project.clj
@@ -4,13 +4,13 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[bidi "2.1.6"]
-                 [camel-snake-kebab "0.4.0"]
+                 [camel-snake-kebab "0.4.1"]
                  [integrant "0.7.0"]
                  [metosin/ring-http-response "0.9.1"]
-                 [org.clojure/clojure "1.10.0"]
-                 [ring/ring-core "1.7.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
-                 [ring/ring-json "0.4.0"]]
+                 [org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.8.0"]
+                 [ring/ring-jetty-adapter "1.8.0"]
+                 [ring/ring-json "0.5.0"]]
   :main ^:skip-aot minimal-api-lein.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
